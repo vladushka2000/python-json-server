d922926b38a340b01a479489c0b7a48f9f35a1dd
vladushka2000 <nazipovvt@yandex.ru>
2024-11-04 22:34:33 +0500

    ci: Добавлен ci для приложения

--- src/config/app_config.py
+++ src/config/app_config.py
@@ -1,5 +1,8 @@
+import dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
+
+dotenv.load_dotenv()
 
 
 class Config(BaseSettings):
@@ -10,7 +13,7 @@ class Config(BaseSettings):
     app_name: str = Field(description="Название приложения", default="python-json-server")
     app_version: str = Field(description="Версия API", default="v1")
 
-    app_host: str = Field(description="Хост сервиса", default="localhost")
+    app_host: str = Field(description="Хост сервиса", default="0.0.0.0")
     app_port: int = Field(description="Порт сервиса", default=7777)
 
 
